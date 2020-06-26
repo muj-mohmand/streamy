90f0822930b59bf2099b5a0fa78f33bf6303f884
Muj <mujmohmand@gmail.com>
2020-06-26 00:51:52 -0400

    added client folder

---
+++ client/src/components/streams/StreamCreate.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import {connect} from 'react-redux';
+import {createStream} from '../../actions';
+import StreamForm from './StreamForm';
+
+class StreamCreate extends React.Component{
+
+
+
+    onSubmit=(formValues) =>{
+        // event.preventDefault(); redux for handlesubmit automatically does this for you.
+        this.props.createStream(formValues);    
+    };
+
+    render(){
+        return(
+            <div>
+                <h3>Create a Stream</h3>   
+                <StreamForm onSubmit={this.onSubmit} /> 
+            </div>
+        );
+    }
+}
+
+
+
+
+
+
+export default connect(null, {createStream})(StreamCreate);
